73362dbdfe13659be787b00c3d8bf5085c49567a
Your Name <you@example.com>
2024-01-10 11:54:32 -0300

    finished

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import WeatherApp from "./components/weatherApp";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1 className="title">Weather App</h1>
+      </div>
+      <WeatherApp></WeatherApp>
+    </div>
+
+  );
+}
+
+export default App;
